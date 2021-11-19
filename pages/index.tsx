30f61f0297e3c5f83f1ae9dc4199fcb5b7d03167
miguel-rosa <miguelgoncvs@gmail.com>
2021-11-19 17:52:16 -0300

    feat: Change texts, add meta tags and videos

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,24 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <img alt="Amigos do planeta" className={styles.logo} src="/amigos-do-planeta.png"/>
+        <img alt="Amigos do planeta" className={styles.logo} src="/amigos-do-planeta.png" />
       </header>
       <div className={styles.hero}>
-    
-      <h1 className={styles.title}>
-        <span className={styles.preText}>
-          Aquecimento Global:
-        </span>
-        O que é, suas consequências e como reduzir o impacto ambiental </h1>
+        <h1 className={styles.title}>
+          <span className={styles.preText}>
+            Aquecimento Global:
+          </span>
+          O que é, suas consequências e como reduzir o impacto ambiental
+        </h1>
+
       </div>
-        
+
       <main className={styles.main}>
         <h2> O que é </h2>
         <p> O aquecimento global é o aumento da temperatura do planeta provocado pelo efeito estufa.
           O efeito estufa acontece porque os humanos poluem o planeta com gases do tóxicos que impedem o calor gerado pelo sol de sair da terra.
         </p>
-        <img alt="Planeta" src="/planeta-2.png"/>
+        <img alt="Planeta" src="/planeta-2.png" />
         <h2> Suas consequências</h2>
         <p> Com o aquecimento global as altas temperaturas fazem as calotas polares derreterem e os nossos amigos ursos polares, pinguins, elefantes marinhos perderem suas casas.Além disso o derretimento de todo esse gelo faz com que tenha mais água no oceano e o seu nível suba alagando várias cidades litorâneas nossas.
         Muitas cordilheiras no mundo estão derretendo e perdendo suas camadas de neve e por isso muitas pessoas vão ter que parar de esquiar em breve.Os Alpes na França e na Suíça já perderam cerca de 40% de sua neve.
@@ -37,15 +38,19 @@ const Home: NextPage = () => {
         </p>
         <img alt="Planeta" src="/planeta-1.png"/>
         <ol type = "1">
-         <li>Economizar água</li>
-         <li>Evite o consumo exagerado de energia</li>
-         <li>Separar os lixos orgânicos e recicláveis</li>
-         <li>Diminuir o uso de automóveis</li>
-         <li>Consumir apenas o necessário e evitar compras compulsivas</li>
-         <li>Utilizar produtos ecológicos e biodegradáveis</li>
-         <li>Não jogar lixos nas ruas</li>
-        </ol>    
-        <img alt="Protesto" src="/protesto.png"/> 
+        <li>Economizar água</li>
+        <li>Evite o consumo exagerado de energia</li>
+        <li>Separar os lixos orgânicos e recicláveis</li>
+        <li>Diminuir o uso de automóveis</li>
+        <li>Consumir apenas o necessário e evitar compras compulsivas</li>
+        <li>Utilizar produtos ecológicos e biodegradáveis</li>
+        <li>Não jogar lixos nas ruas</li>
+        </ol>
+        <img alt="Protesto" src="/protesto.png"/>
+        <div className={styles.videos}>
+          <iframe src="https://player.vimeo.com/video/647457455" className={styles.video}/>
+          <iframe src="https://player.vimeo.com/video/647512542" className={styles.video}/>
+        </div>
       </main>
     </div>
   )
